4f9ea30b5f7aaeda4474b6b8fd7237ddd90a42e4
Stefan Brechbühl <mail@stebre.ch>
2025-09-12 10:41:32 +0200

    change _includes > _templates

--- eleventy.config.js
+++ eleventy.config.js
@@ -148,7 +148,7 @@ export const config = {
 	// These are all optional:
 	dir: {
 		input: "content",          // default: "."
-		includes: "../_includes",  // default: "_includes" (`input` relative)
+		includes: "../_templates",  // default: "_includes" (`input` relative)
 		data: "../_data",          // default: "_data" (`input` relative)
 		output: "_site"
 	},
